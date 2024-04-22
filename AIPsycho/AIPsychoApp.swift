c27c5a262b440a7daec75a361f558551cea61661
Kirill Ponomarenko <ponomarenko.k@techspaceapp.com>
2024-04-22 23:56:35 +0300

    Initial Commit

---
+++ AIPsycho/AIPsychoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPsychoApp.swift
+//  AIPsycho
+//
+//  Created by Kirill Ponomarenko on 22.04.24.
+//
+
+import SwiftUI
+
+@main
+struct AIPsychoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
